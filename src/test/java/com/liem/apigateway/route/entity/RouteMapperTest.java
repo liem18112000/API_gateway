cfa2a91ccbb0e8aba3249bdede11b7e880731005
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-03-05 15:49:03 +0700

    Add test and docker

---
+++ src/test/java/com/liem/apigateway/route/entity/RouteMapperTest.java
@@ -0,0 +1,34 @@
+package com.liem.apigateway.route.entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouteMapperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void toDto() {
+    }
+
+    @Test
+    void toDtoWithException() {
+    }
+
+    @Test
+    void toEntity() {
+    }
+
+    @Test
+    void toEntityWithException() {
+    }
+}
