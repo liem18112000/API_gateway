cc21670e12fce13fc89ddeb3de40662bf0bc79fb
Đoàn Văn Thanh Liêm <liem18112000@gmail.com>
2022-04-05 10:08:39 +0700

    Update static route service

--- src/main/java/com/liem/apigateway/route/service/impl/StaticRouteService.java
+++ src/main/java/com/liem/apigateway/route/service/impl/StaticRouteService.java
@@ -15,11 +15,13 @@ public class StaticRouteService implements RouteService {
 
     @SneakyThrows
     private String getUri(@NotBlank final String domain) {
-        return "lb://".concat(domain).concat("/**");
+        return "lb://"
+                .concat(domain.trim().replaceAll("endpoint/", ""))
+                .concat("-service/**");
     }
 
     private String getPath(@NotBlank final String domain) {
-        return "/".concat(domain).concat("/**");
+        return "/".concat(domain.trim()).concat("/**");
     }
 
     /**
@@ -27,17 +29,27 @@ public class StaticRouteService implements RouteService {
      */
     public Flux<RouteDTO> getAllRoutes() {
         return Flux.fromArray(new String[]{
-                "ingredient", "recipe", "supplier",
-                "notification", "security"
+                "ingredient", "recipe", "supplier", "notification", "security",
+                "endpoint/ingredient", "endpoint/recipe", "endpoint/supplier", "endpoint/notification"
         }).map(this::getRouteDTO);
     }
 
     private RouteDTO getRouteDTO(String domain) {
         final var path = getPath(domain);
         final var uri = getUri(domain);
-        log.info("Mapping {} - {}", uri, path);
-        return RouteDTO.builder().serviceName(domain).path(path)
-                .uri(uri).active(true).auth(false).build();
+        final var isAuth = getAuth(domain);
+        log.info("Mapping {} - {} - {} - {}", domain, uri, path, isAuth);
+        return RouteDTO.builder()
+                .serviceName(domain)
+                .path(path)
+                .uri(uri)
+                .active(true)
+                .auth(isAuth)
+                .build();
+    }
+
+    private boolean getAuth(String domain) {
+        return !domain.contains("endpoint") && !domain.contains("security");
     }
 
 
